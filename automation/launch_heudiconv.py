f3f37446cdfa89b6e3d5b8fbf0d76064c45ac060
alicewuhhh <71121323+alicewuhhh@users.noreply.github.com>
2023-07-24 16:30:14 -0400

    `

--- automation/launch_heudiconv.py
+++ automation/launch_heudiconv.py
@@ -32,6 +32,7 @@ for session in sessions:
     print(session.label)
 
     # check if heudiconv has already been run
+    # comment out if it is not running
     for analysis in session.analyses:
         states = ["complete", "queued", "running", "failed"]
         if "heudiconv" in analysis.label and any(analysis.job['state'] == s for s in states):
